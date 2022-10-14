5c9f90cf4f49b45138b3e50d1e4cbd789fa6e6b1
Manuel <jmanuel.dealava@gmail.com>
2022-10-13 23:45:05 -0300

    For loops to create buttons and rows

--- src/App.js
+++ src/App.js
@@ -1,62 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
-import * as Tone from 'tone';
+import logo from "./logo.svg";
+import "./App.css";
+import * as Tone from "tone";
 
 function App() {
   return (
     <div className="App">
-          <Board name="Push1"/>
-      </div>
+      <Board name="Push1" />
+    </div>
   );
 }
 
-function Board(props){
-    return (
-        //TODO poner un for para crear 8 rows
-            <div>
-            <Row name="Row1"/>
-            </div>
-    );
+let rowList = [];
+for (let i = 0; i < 8; i++) {
+    rowList.push(<Row name={i+1}></Row>)
+}
+
+function Board(props) {
+  return (
+    //TODO poner un for para crear 8 rows
+    <div>
+      {rowList}
+    </div>
+  );
 }
 
 
-<Button name="1" color="red" note="a"/>
-let buttons=['Item 1','Item 2','Item 3','Item 4','Item 5'];
-let buttonList=[];
-buttons.forEach((item,index)=>{
-  buttonList.push( <Button name={item}></Button>)
-})
+let buttonList = [];
 
-function Row(props){
-    return (    
-        //TODO poner un for para crear 8 botons
-<div className="pushRow">
-            <div id="button1">
-                {buttonList}
-            </div>
-            
-          
-</div>
-    );
+for (let i = 0; i < 8; i++) {
+    buttonList.push(<Button name={i+1}></Button>)
+};
+
+
+
+function Row(props) {
+  return (
+    //TODO poner un for para crear 8 botons
+    <div className="pushRow">{buttonList}</div>
+  );
 }
 
-function Button(props){
+function Button(props) {
+  const handleClick = (e) => {
+    e.preventDefault();
 
-    const handleClick = (e) => {
-        e.preventDefault();
+    const synth = new Tone.Synth().toDestination();
+    synth.triggerAttackRelease("C4", "8n");
 
-        const synth = new Tone.Synth().toDestination();
-        synth.triggerAttackRelease("C4", "8n");
+    console.log("The button was clicked.");
+  };
 
-        console.log('The button was clicked.');
-    };
-
-
-    return (
-           <div>
-            <p className="pushButton" onClick={handleClick}> I am {props.name}</p>
-            </div>
-);
+  return (
+    <div>
+      <p className="pushButton" onClick={handleClick}>
+        {" "}
+        I am {props.name}
+      </p>
+    </div>
+  );
 }
 
 export default App;
