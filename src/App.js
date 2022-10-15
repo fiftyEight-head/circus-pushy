23d8b000a12f5949bdd3d922b37c307dcd6fc86f
Manuel <jmanuel.dealava@gmail.com>
2022-10-15 09:25:24 -0300

    added note generation for each button

--- src/App.js
+++ src/App.js
@@ -9,16 +9,17 @@ function App() {
   );
 }
 
-let buttonsMatrix = [];
+
+let buttonsRow = [];
 for (let i = 0; i < 8; i++) {
-    buttonsMatrix.push(<Row rowId={i+1}></Row>)
+    buttonsRow.push(<Row rowId={i+1}></Row>)
 }
 
 function Board(props) {
   return (
     //TODO poner un for para crear 8 rows
     <div className="buttonBoard">
-      {buttonsMatrix}
+      {buttonsRow}
     </div>
   );
 }
@@ -31,7 +32,7 @@ function Row(props) {
   let buttonList = [];
 
   for (let i = 0; i < 8; i++) {
-      buttonList.push(<Button rowId={props.rowId} name={i+1}></Button>)
+      buttonList.push(<Button rowId={props.rowId} name={i+1} note={calculateNote(props.rowId, i+1)}></Button>)
     };
   
   return (
@@ -40,12 +41,107 @@ function Row(props) {
   );
 }
 
+function calculateAbsoluteButtonNumber(rowId, buttonId){
+  return rowId * buttonId;
+}
+
+function calculateNote(rowId, buttonId){
+
+  let finalNote = ""
+  switch (rowId) {
+    case 1: finalNote += "A";
+      
+      break;
+
+      case 2: finalNote += "B";
+      
+      break;
+      
+      case 3: finalNote += "C";
+      
+      break;
+      
+      case 4: finalNote += "D";
+      
+      break;
+      
+      case 5: finalNote += "E";
+      
+      break;
+      
+      case 6: finalNote += "F";
+      
+      break;
+      
+      case 7: finalNote += "G";
+      
+      break;
+      
+      case 8: finalNote += "H";
+      
+      break;
+      
+  
+    default: finalNote += "A";
+      break;
+
+  }
+
+  switch (buttonId) {
+    case 1: finalNote += "1";
+      
+      break;
+
+      case 2: finalNote += "2";
+      
+      break;
+      
+      case 3: finalNote += "3";
+      
+      break;
+      
+      case 4: finalNote += "4";
+      
+      break;
+      
+      case 5: finalNote += "5";
+      
+      break;
+      
+      case 6: finalNote += "6";
+      
+      break;
+      
+      case 7: finalNote += "7";
+      
+      break;
+      
+      case 8: finalNote += "8";
+      
+      break;
+      
+    default: finalNote += "1";
+      break;
+
+  }
+
+  console.log("CREANDO NOTA:" + finalNote);
+
+  return finalNote;
+}
+
+function calculateColor(absoluteButtonNumber){
+  return 0;
+}
+
+
 function Button(props) {
+
   const handleClick = (e) => {
     e.preventDefault();
-
+    //FIXME enviar synth via props o state.
     const synth = new Tone.Synth().toDestination();
-    synth.triggerAttackRelease("C4", "8n");
+    synth.triggerAttackRelease(props.note, "8n");
 
     console.log("The button was clicked.");
   };
